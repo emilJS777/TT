3fc4050ea7548a2ca739a0065899f23d3eb8529d
emiljs777 <e.pargevich@mail.ru>
2023-03-21 21:28:08 +0400

    sdsfd

--- src/Service/ServiceValidator.py
+++ src/Service/ServiceValidator.py
@@ -3,7 +3,15 @@ service_schema = {
     "properties": {
         "title": {"type": "string", "minLength": 4, "maxLength": 40},
         "short_description": {"type": "string", "maxLength": 400},
-        "long_description": {"type": "string", "maxLength": 9000}
+        "long_description": {"type": "string", "maxLength": 9000},
+        "name": {"type": "string", "maxLength": 400},
+        "last_name": {"type": "string", "maxLength": 400},
+        "patronymic": {"type": "string", "maxLength": 400},
+        "data": {"type": "string", "maxLength": 400},
+        "city": {"type": "string", "maxLength": 400},
+        "gender": {"type": "string", "maxLength": 400},
+        "problem": {"type": "string", "maxLength": 400},
+        "exercise": {"type": "string", "maxLength": 400},
       },
     "required": []
 }
